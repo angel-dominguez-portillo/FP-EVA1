75a18209bf160a910b2cc56cb2632343f72f5087
angel-dominguez-portillo <L24550319@chihuahua2.tecnm.mx>
2024-09-09 16:48:36 -0600

    Add files via upload

---
+++ EVA1_4_TELEVISION/src/eva1_4_televicion/EVA1_4_TELEVICION.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_4_televicion;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_4_TELEVICION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String marca;
+        String modelo;
+        String tecnologia;
+        int tama;
+        double precio;
+        
+        //NOMBRAMOS LA VARIABLES
+        marca = "Sansung";
+        modelo = "Crystal UHD";
+        tecnologia = "LED";
+        tama = 76;
+        precio = 40000;
+        
+        //IMPRIMIMOS LA VARIABLES
+        System.out.print(marca);
+        System.out.print(modelo);
+        System.out.print(tecnologia);
+        System.out.print(tama);
+        System.out.print(precio);
+    }
+    
+}
